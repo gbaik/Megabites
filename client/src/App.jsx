03a39eb74eee929a643c32a6cd51870163eb8a7e
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-11-18 02:22:02 -0800

    Wrap app in MuiThemeProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Home from './pages/Home/Home.jsx';
 import Donor from './pages/Donor/Donor.jsx';
 import Bank from './pages/Bank/Bank.jsx';
+import { List, ListItem } from 'material-ui';
 
 
 export default class App extends Component {
 
   render() {
     return (
-      <div>
+      <MuiThemeProvider>
         <h1>App</h1>
         <Home />
+        <List>
+          <ListItem primaryText="some Shit" />
+        </List>
         <Donor />
         <Bank />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
